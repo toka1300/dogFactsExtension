1b8dd299885f0f4b4257744c22a91f3cc428f8f2
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-07-10 21:42:18 -0400

    Fully functional

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let dogFact;
+const factElement = document.querySelector('.fact span');
+const pawButton = document.querySelector('svg');
+
+const fetchDogFact = async () => {
+  try {
+    const resp = await fetch('https://dogapi.dog/api/v2/facts');
+    const json = await resp.json()
+    const fact = json.data[0].attributes.body
+    return fact
+  } catch (e) {
+    console.log('We have it an error', e);
+  }
+}
+
+const insertDogFact = async() => {
+  const dogFact = await fetchDogFact()
+  factElement.textContent = dogFact;
+}
+
+pawButton.addEventListener('click', () => {
+  insertDogFact()
+})
